7ef88364088b3f8a5227bf5215d143b5549dcc14
Ali Sbane <ali.sbane.dev@gmail.com>
2024-04-13 09:29:16 +0000

    Batman

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+// React initial import
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// components
+import App from "./App";
+
+// bind to the root id element, and render the react app inside
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
