efb65e8f3eb1334aae2ff5d30090f5bfea2cab5d
Ali Sbane <ali.sbane.dev@gmail.com>
2024-04-13 11:18:35 +0000

    Made the tailwindcss work after some husle

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import ReactDOM from "react-dom/client";
 // components
 import App from "./App";
 
+// main css
+import "./main.css";
+
 // bind to the root id element, and render the react app inside
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
