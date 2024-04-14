43adf3eea8fff95b5622aa329540f7c4a6ec3068
Ali Sbane <ali.sbane.dev@gmail.com>
2024-04-14 12:25:25 +0100

    Making header position absolute, I think this the way to achieve the design layout

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="mt-5 md:mt-[29px] ">
+    <div className="absolute mt-5 w-full md:mt-[29px]">
       <div className="mx-auto flex max-w-[153px] flex-col items-center gap-5 md:max-w-[708px] md:flex-row md:justify-between xl:mt-[39px] xl:max-w-[1110px] xl:pr-[29px]">
         {/* Logo */}
         <a
