0e22111ecc19dab5cdc6cd5ac4dcc438e35beac8
Ali Sbane <ali.sbane.dev@gmail.com>
2024-04-15 09:57:49 +0100

    Hero section for mobile devices pixel-perfect to the mobile design, done!

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC = () => {
         backgroundPosition: "left top 124px, right top 254px",
       }}
     >
-      <div className="flex flex-col items-center xl:max-w-[1110px]">
+      <div className="flex flex-col items-center px-4 xl:max-w-[1110px]">
         {/* Picture */}
         <div className="bg-gradient-to-b from-[rgba(36,36,36,0.0001)] to-[rgba(36,36,35,1)] pt-[141px]">
           <picture>
@@ -21,6 +21,29 @@ const Hero: React.FC = () => {
         </div>
 
         {/* Text Part */}
+        <div className="mx-auto mt-10 max-w-[343px]">
+          <div className="relative">
+            <h1 className="text-center text-[40px] font-bold leading-[40px] tracking-[-1.14px]">
+              Nice to meet you! I’m Adam Keyes.
+            </h1>
+            <span className="absolute -bottom-[4px] right-8 h-[4px] w-[225px] bg-green" />
+          </div>
+
+          <p className="mt-6 text-center font-medium leading-[26px]">
+            Based in the UK, I’m a front-end developer passionate about building
+            accessible web apps that users love.
+          </p>
+
+          <div className="mt-6 text-center">
+            <a
+              className="relative mx-auto font-bold leading-7 tracking-[2.29px]"
+              href="#contactme"
+            >
+              CONTACT ME
+              <span className="absolute bottom-[-14px] left-0 h-[2px] w-full bg-green" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
