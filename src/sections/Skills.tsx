979d247aba4a55383374a45d57f3748f270ffb9b
Ali Sbane <ali.sbane.dev@gmail.com>
2024-04-22 18:20:15 +0100

    Skills data moved outside of the component, removed padding bottom from the skills div on md breakpoint and above to make the next section pixel-perfect to the design

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -3,21 +3,21 @@ import React from "react";
 // Graphics
 import linesDonut_PNG from "../assets/graphics/lines-donut.png";
 
+// skills array
+// [skill, yearsOfExperience]
+const data = [
+  ["HTML", 4],
+  ["CSS", 4],
+  ["Javascript", 4],
+  ["Accessibility", 4],
+  ["React", 3],
+  ["Sass", 3],
+];
 const Skills: React.FC = () => {
-  // skills array
-  // [skill, yearsOfExperience]
-  const data = [
-    ["HTML", 4],
-    ["CSS", 4],
-    ["Javascript", 4],
-    ["Accessibility", 4],
-    ["React", 3],
-    ["Sass", 3],
-  ];
-
   return (
     <section className="mt-20  md:mt-[60px] xl:mt-[104px]">
       <div className="relative mx-auto max-w-[1100px]">
+        {/*  linesDonut */}
         <div className="absolute -right-[344px] top-[540px] md:-right-[332px] md:top-[387px] xl:-right-[404px] xl:top-[253px]">
           <picture>
             <img
@@ -28,8 +28,9 @@ const Skills: React.FC = () => {
           </picture>
         </div>
 
-        <div className="mx-auto flex max-w-[345px] flex-col gap-6 border-y py-10 text-center md:max-w-[708px] md:flex-row md:flex-wrap md:gap-0 md:gap-x-[7px] md:gap-y-[52px] md:border-b-0 md:py-[52px] xl:max-w-none xl:gap-x-[30px] xl:gap-y-[58px] xl:pt-[72px]">
-          {/* Looping over the data and rendering */}
+        {/* Skills Grid */}
+        <div className="mx-auto flex max-w-[345px] flex-col gap-6 border-y py-10 text-center md:max-w-[708px] md:flex-row md:flex-wrap md:gap-0 md:gap-x-[7px] md:gap-y-[52px] md:border-b-0 md:pb-0 md:pt-[52px] xl:max-w-none xl:gap-x-[30px] xl:gap-y-[58px] xl:pt-[72px]">
+          {/* Looping over the skills data and rendering */}
           {data.map(([skill, yearsOfExperience]) => (
             <div
               key={skill}
