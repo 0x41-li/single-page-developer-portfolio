32df3bc651a00e5bd0145cb3bc46f595d90bc154
Ali Sbane <ali.sbane.dev@gmail.com>
2024-04-26 17:11:18 +0100

    Removed the big height used for dev purposes, and also added a todo comment

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import Hero from "./sections/Hero";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
 import ContactMe from "./sections/ContactMe";
+import Footer from "./sections/Footer";
 
 const App: React.FC = () => {
   return (
-    <div className="h-[8000px] overflow-hidden">
+    // TODO: Remove the overflow hidden, and handle the overflow on the sections levels
+    <div className="overflow-hidden">
       <Header />
       <Hero />
       <Skills />
       <Projects />
       <ContactMe />
+      <Footer />
     </div>
   );
 };
