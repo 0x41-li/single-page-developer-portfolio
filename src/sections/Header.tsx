f8fd454d6331c45671dbe9ef6b7d4ecbc66c379f
Ali Sbane <ali.sbane.dev@gmail.com>
2024-05-08 23:43:01 +0100

    Removed gsap, and add animejs instead, it's free!

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,4 @@
-import React, { useRef } from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import React from "react";
 
 // components
 import IconLink from "../components/IconLink";
@@ -8,26 +6,12 @@ import IconLink from "../components/IconLink";
 // data
 import data from "../data/data.json";
 
-// Global code
-gsap.registerPlugin(useGSAP);
 
 const Header: React.FC = () => {
   const { iconLinks } = data.header;
 
-  const headerRef = useRef(null);
-
-  useGSAP(() => {
-    gsap.from(headerRef.current, {
-      duration: 1,
-      y: -100,
-      opacity: 0,
-      ease: "power2.inOut",
-    });
-  });
-
   return (
     <header
-      ref={headerRef}
       className="absolute z-50 mt-5 w-full md:mt-[29px] xl:mt-[39px] "
     >
       <div className="mx-auto flex max-w-[153px] flex-col items-center gap-5 md:max-w-[708px] md:flex-row md:justify-between xl:max-w-[1110px] xl:pr-[29px]">
